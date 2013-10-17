5aa6d00fa0c5d0ddd802ad9b14a4618ff484ace7
Daniel Uribe Ayvar <danieluribeayvar@gmail.com>
2013-10-17 12:24:19 -0500

    added an Area Object instead of Direccion
    The Area object will be use for manage herarchical organization

---
+++ Activos/Enums/NivelArea.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Activos.Enums
+{
+    public enum NivelArea
+    {
+        DIRECCION,
+        DEPARTAMENTO,
+        AGENCIA
+    }
+}
---
+++ Activos/Modelo/Area.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Activos.Enums;
+
+namespace Activos.Modelo
+{
+    public class Area : BaseBO
+    {
+        private Area subArea;
+        private NivelArea nivelArea;
+
+        public Area() { }
+
+        public Area(int _id, String _descripcion) : base(_id, _descripcion) { }
+
+        public Area(Area _subArea)
+        {
+            this.SubArea = _subArea;
+        }
+
+        public Area(Area _subArea, NivelArea _nivelArea)
+        {
+            this.SubArea = _subArea;
+            this.NivelArea = _nivelArea;
+        }
+
+        public Area(int _id, String _descripcion, Area _subArea, NivelArea _nivelArea)
+            : base(_id, _descripcion) 
+        {
+            this.SubArea = _subArea;
+            this.NivelArea = _nivelArea;
+        }
+
+        public Area SubArea
+        {
+            get { return subArea; }
+            set { this.subArea = value; }
+        }
+
+        public NivelArea NivelArea
+        {
+            get { return nivelArea; }
+            set { this.nivelArea = value; }
+        }
+    }
+}
