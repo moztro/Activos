2d22bbeef9ccd7a3351564e4ebe023c6f92a96f7
Daniel Uribe Ayvar <danieluribeayvar@gmail.com>
2013-10-17 11:47:26 -0500

    more model definition

---
+++ Activos/Modelo/Direccion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Activos.Modelo
+{
+    public class Direccion : BaseBO
+    {
+        private BaseBO departamento;
+    }
+}
