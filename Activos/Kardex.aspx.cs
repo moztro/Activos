d880cffa42310a87dff50387062c949d8e4a0844
kelvin088 <kelvinmgd88@gmail.com>
2013-10-23 09:58:02 -0500

    Formato Kardex

--- Activos/Kardex.aspx.cs
+++ Activos/Kardex.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +10,32 @@ namespace Activos
 {
     public partial class Kardex : System.Web.UI.Page
     {
+        DataTable tabla = new DataTable();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            tabla.Columns.Add("concepto");
+            tabla.Columns.Add("remFac");
+            tabla.Columns.Add("invIni");
+            tabla.Columns.Add("costoIni");
+            tabla.Columns.Add("dev");
+            tabla.Columns.Add("compras");
+            tabla.Columns.Add("preciounit");
+            tabla.Columns.Add("iva");
+            tabla.Columns.Add("costoUnit");
+            tabla.Columns.Add("total");
+            tabla.Columns.Add("promedio");
+            tabla.Columns.Add("prog");
+            tabla.Columns.Add("vales");
+            tabla.Columns.Add("salidas");
+            tabla.Columns.Add("invFinal");
+            tabla.Columns.Add("costInvFinal");
+
+            tabla.Rows.Add("1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1");
+
+            gvRegistros.DataSource = tabla;
+            gvRegistros.DataBind();
         }
     }
 }
